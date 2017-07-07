beb0775971e350722e453ddf16e338d4fab29bcf
mbouchenoire <gitbouchenoire@gmail.com>
2017-07-07 18:17:20 +0200

    Improve error handling

--- pubroadcaster/main.py
+++ pubroadcaster/main.py
@@ -29,33 +29,36 @@ class Main(object):
                 log.info("scaning players: " + ",".join(self.profile_names) + " (interval: " + str(self.tracking_interval) + "s)...")
 
                 for profile_name in self.profile_names:
-                    await asyncio.sleep(1) # the pubgtracker api does not allow more than 1 query per second
-                    log.debug("tracking " + profile_name + "...")
+                    try:
+                        await asyncio.sleep(1) # the pubgtracker api does not allow more than 1 query per second
+                        log.debug("tracking " + profile_name + "...")
+                        profile = self.tracker.get_profile(profile_name)
+                        known_snapshot = profile_name in self.snapshots
+                        snapshot = self.snapshots[profile_name] if known_snapshot else profile
+
+                        for stats in profile.Stats:
+                            region = stats["Region"]
+                            season = stats["Season"]
+                            mode = stats["Match"]
+
+                            win = profile.has_incremented_from(snapshot, region, season, mode, u'Wins')
+                            topten = profile.has_incremented_from(snapshot, region, season, mode, u'Top10s')
+
+                            message = None
+
+                            if win and region != "agg":
+                                message = MessageBuilder().build_win(profile_name, snapshot, profile, region, season, mode)
+                            elif topten and region != "agg":
+                                message = MessageBuilder().build_topten(profile_name, snapshot, profile, region, season, mode)
+                                
+                            if message is not None:
+                                log.info(message)
+                                await discord_client.send_message(broadcast_channel, message)
+
+                        self.snapshots[profile_name] = profile
+                    except expression as identifier:
+                        log.error(identifier)
                     
-                    profile = self.tracker.get_profile(profile_name)
-                    known_snapshot = profile_name in self.snapshots
-                    snapshot = self.snapshots[profile_name] if known_snapshot else profile
-
-                    for stats in profile.Stats:
-                        region = stats["Region"]
-                        season = stats["Season"]
-                        mode = stats["Match"]
-
-                        win = profile.has_incremented_from(snapshot, region, season, mode, u'Wins')
-                        topten = profile.has_incremented_from(snapshot, region, season, mode, u'Top10s')
-
-                        message = None
-
-                        if win and region != "agg":
-                            message = MessageBuilder().build_win(profile_name, snapshot, profile, region, season, mode)
-                        elif topten and region != "agg":
-                            message = MessageBuilder().build_topten(snapshot, profile)
-                            
-                        if message is not None:
-                            log.info(message)
-                            await discord_client.send_message(broadcast_channel, message)
-
-                    self.snapshots[profile_name] = profile
 
         discord_client.loop.create_task(check_stats_task())
         discord_client.run(self.discord_bot_token)
